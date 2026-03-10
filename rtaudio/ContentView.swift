77441af2a679e8ddd61023e7147bc336a5c562d3
zeph <zephyrzefa15@gmail.com>
2026-03-10 15:00:40 +0100

    Initial Commit

---
+++ rtaudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rtaudio
+//
+//  Created by zeph on 10/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rtaudio/rtaudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  rtaudioApp.swift
+//  rtaudio
+//
+//  Created by zeph on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct rtaudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
